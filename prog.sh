13621eec3aafd43b4be819bb078207f140e0e09f
SIS-E <sistemas@empak.com.mx>
2026-02-11 13:15:11 -0600

    Subiendo archivo sh
    Este archivo es una prueba

---
+++ prog.sh
@@ -0,0 +1,21 @@
+#programa prueba
+
+#solicita el primer numero
+
+read -p "Digita el Primer numero: " num1
+
+#Solicitar el segundo numero
+
+read -p "Digita el Segundo numero: " num2
+
+#Realizar la suma usando la expansón aritmética
+
+suma=$((num1 + num2))
+
+#Mostrar el resultado
+
+echo "La suma de $num1 y $num2 es: $suma"
+
+echo "Presiona ENTER para continuar...."
+
+ read
