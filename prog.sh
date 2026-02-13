70df34fcd95c94c0f13cd50c9ef0a19b6d1d8047
SIS-E <sistemas@empak.com.mx>
2026-02-13 12:44:38 -0600

    Cambiando de suma a resta

--- prog.sh
+++ prog.sh
@@ -8,14 +8,14 @@ read -p "Digita el Primer numero: " num1
 
 read -p "Digita el Segundo numero: " num2
 
-#Realizar la suma usando la expansón aritmética
+#Realizar la resta usando la expansón aritmética
 
-suma=$((num1 + num2))
+rest=$((num1 - num2))
 
 #Mostrar el resultado
 
-echo "La suma de $num1 y $num2 es: $suma"
+echo "La resta de $num1 y $num2 es: $rest"
 
-echo "Presiona ENTER para continuar.... esto es un buen programa"
+echo "Presiona ENTER para continuar...."
 
  read
